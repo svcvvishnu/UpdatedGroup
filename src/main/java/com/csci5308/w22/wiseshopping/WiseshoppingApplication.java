99a4a6ecca527e228c4d602497276800cb8516d3
Elizabeth <elizabeth.james@dal.ca>
2022-02-18 03:20:06 -0400

    initializing spring project

---
+++ src/main/java/com/csci5308/w22/wiseshopping/WiseshoppingApplication.java
@@ -0,0 +1,15 @@
+package com.csci5308.w22.wiseshopping;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseshoppingApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseshoppingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/csci5308/w22/wiseshopping/WiseshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci5308.w22.wiseshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseshoppingApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
