e42790b1f50b0a0a57897548e69404a11c3031a1
Elizabeth <elizabeth.james@dal.ca>
2022-02-20 00:42:56 -0400

    issue #5 | introduce runner

--- src/main/java/com/csci5308/w22/wiseshopping/WiseshoppingApplication.java
+++ src/main/java/com/csci5308/w22/wiseshopping/WiseshoppingApplication.java
@@ -2,8 +2,10 @@ package com.csci5308.w22.wiseshopping;
 
 
 
+import com.csci5308.w22.wiseshopping.runner.Runner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WiseshoppingApplication {
@@ -11,5 +13,10 @@ public class WiseshoppingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WiseshoppingApplication.class, args);
+    }
+
+    @Bean
+    public Runner getRunner(){
+        return new Runner();
     }
 }
